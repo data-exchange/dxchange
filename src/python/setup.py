766bd4177e0a89164d0da5f379f19fd012258346
Francesco De Carlo <decarlof@gmail.com>
2014-03-01 19:11:41 -0600

    added set up

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,18 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import io
+import platform
+import warnings
+
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, Extension, find_packages
+
+# Check Python packages.
 try:
-	from setuptools import setup
+    import numpy
 except ImportError:
-	from distutils.core import setup
+    raise ImportError("tomopy requires numpy 1.8.0 (hint: pip install numpy)")
+try:
+    import h5py
+except ImportError:
+    raise ImportError("tomopy requires h5py 2.2.1 (hint: pip install h5py)")
 
-config = {
-	'description': 'Data Exchange file API',
-	'author': 'David J. Vine',
-	'url': 'https://github.org/djvine/data_exchange',
-	'author_email': 'djvine@gmail.com',
-	'version': '0.1',
-	'install_requires': ['nose'],
-	'packages': ['data_exchange'],
-	'scripts': [],
-	'name': 'data_exchange'
-}
+# Main setup configuration.
+setup(
+        name='data-exchange',
+        version='0.0.1',
 
-setup(**config)
+        packages = find_packages(),
+        include_package_data = True,
+
+        # Specify C/C++ file paths. They will be compiled and put into tomopy.lib:
+        # ext_modules=[ext_fftw, ext_gridrec],
+
+        author='Francesco De Carlo',
+        author_email='decarlo@aps.anl.gov',
+
+        description='Data Exchange Toolbox',
+        keywords=['tomography', 'data', 'format'],
+        url='http://aps.anl.gov/DataExchange',
+        download_url='http://github.com/data-exchange/data-exchange',
+
+        license='BSD',
+        platforms='Any',
+
+        classifiers=['Development Status :: 1 - Beta',
+                     'License :: OSI Approved :: BSD License',
+                     'Intended Audience :: Science/Research',
+                     'Intended Audience :: Education',
+                     'Intended Audience :: Developers',
+                     'Natural Language :: English',
+                     'Operating System :: OS Independent',
+                     'Programming Language :: Python',
+                     'Programming Language :: Python :: 2.6',
+                     'Programming Language :: Python :: 2.7']
+)
