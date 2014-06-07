92517861f3912034a741693ef3ceaea213a462f6
Francesco De Carlo <decarlof@gmail.com>
2014-06-06 23:02:34 -0500

    added support for direct tomoPy import for CHESS data

--- demo/convert_CHESS.py
+++ demo/convert_CHESS.py
@@ -14,38 +14,49 @@ import dataexchange.xtomo.xtomo_exporter as ex
 
 def main():
 
-    file_name = '/local/dataraid/databank/CHESS/Dummy001_.tif'
-    hdf5_file_name = '/local/dataraid/databank/dataExchange/microCT/CHESS_01.h5'
+    file_name = '/local/dataraid/databank/CHESS/scan1/scan1_.tiff'
+    dark_file_name = '/local/dataraid/databank/CHESS/scan1/scan1_dark_.tiff'
+    white_file_name = '/local/dataraid/databank/CHESS/scan1/scan1_white_.tiff'
+
+    hdf5_file_name = '/local/dataraid/databank/dataExchange/microCT/CHESS_02.h5'
     sample_name = 'Dummy'
 
     projections_start = 1
-    projections_end = 181
+    projections_end = 361
     white_start = 0
-    white_end = 10
+    white_end = 1
     white_step = 1
     dark_start = 0
-    dark_end = 10
+    dark_end = 1
     dark_step = 1
+
+
+    # set to convert slices between slices_start and slices_end
+    # if omitted all data set will be converted   
+    slices_start = 400    
+    slices_end = 405    
 
     mydata = dx.Import()
     # Read series of images
     data, white, dark, theta = mydata.series_of_images(file_name,
                                                        projections_start = projections_start,
                                                        projections_end = projections_end,
+                                                       slices_start = slices_start,
+                                                       slices_end = slices_end,
                                                        sample_name = sample_name,
-                                                       projections_digits = 4,
+                                                       projections_digits = 3,
                                                        projections_zeros = True,
                                                        log='INFO'
                                                     )    
-    mydata = ex.Export()
-    # Create minimal data exchange hdf5 file
-    mydata.xtomo_exchange(data = data,
-                          data_white = white,
-                          data_dark = dark,
-                          theta = theta,
-                          hdf5_file_name = hdf5_file_name,
-                          data_exchange_type = 'tomography_raw_projections'
-                          )
+##    mydata = ex.Export()
+##    # Create minimal data exchange hdf5 file
+##    mydata.xtomo_exchange(data = data,
+##                          data_white = white,
+##                          data_dark = dark,
+##                          theta = theta,
+##                          hdf5_file_name = hdf5_file_name,
+##                          data_exchange_type = 'tomography_raw_projections'
+##                          )
 
 if __name__ == "__main__":
     main()
--- demo/import_tomoPy_CHESS.py
+++ demo/import_tomoPy_CHESS.py
@@ -4,53 +4,63 @@ import tomopy
 import dataexchange.xtomo.xtomo_importer as dx
 import dataexchange.xtomo.xtomo_exporter as ex
 
+def main():
 
-projections_start = 1
-projections_end = 181
+    file_name = '/local/dataraid/databank/CHESS/scan1/scan1_.tiff'
+    dark_file_name = '/local/dataraid/databank/CHESS/scan1/scan1_dark_.tiff'
+    white_file_name = '/local/dataraid/databank/CHESS/scan1/scan1_white_.tiff'
 
-# read a series of tiff
-file_name = '/local/dataraid/databank/CHESS/Dummy001_.tif'
-mydata = dx.Import()
-# Read series of images
-data, white, dark, theta = mydata.series_of_images(file_name,
-                                                   projections_start = projections_start,
-                                                   projections_end = projections_end,
-                                                   projections_digits = 4,
-                                                   projections_zeros = True,
-                                                   log='INFO'
-                                                )
+    hdf5_file_name = '/local/dataraid/databank/dataExchange/microCT/CHESS_02.h5'
+    sample_name = 'Dummy'
 
-# or convert the series of tiff in data exchange
-##hdf5_file_name = '/local/dataraid/databank/dataExchange/microCT/CHESS_01.h5'
-##sample_name = 'Dummy'
-##mydata = ex.Export()
-### Create minimal data exchange hdf5 file
-##mydata.xtomo_exchange(data = data,
-##                      data_white = white,
-##                      data_dark = dark,
-##                      theta = theta,
-##                      hdf5_file_name = hdf5_file_name,
-##                      data_exchange_type = 'tomography_raw_projections'
-##                      )
-
-# and/or read as data exchange
-# Read HDF5 file.
-##data, white, dark, theta = tomopy.xtomo_reader('/local/dataraid/databank/dataExchange/microCT/CHESS_01.h5',
-##                                               slices_start=100,
-##                                               slices_end=101)
-
-# Xtomo object creation and pipeline of methods.  
-d = tomopy.xtomo_dataset(log='debug')
-d.dataset(data, white, dark, theta)
-d.normalize()
-d.correct_drift()
-d.optimize_center()
-#d.phase_retrieval()
-#d.correct_drift()
-d.center=99.5
-d.gridrec()
+    projections_start = 1
+    projections_end = 361
+    white_start = 0
+    white_end = 1
+    white_step = 1
+    dark_start = 0
+    dark_end = 1
+    dark_step = 1
 
 
-# Write to stack of TIFFs.
-tomopy.xtomo_writer(d.data_recon, 'tmp/CHESS_Integrated_100_', axis=0)
+    # set to convert slices between slices_start and slices_end
+    # if omitted all data set will be converted   
+    slices_start = 400    
+    slices_end = 405    
+
+    mydata = dx.Import()
+    # Read series of images
+    data, white, dark, theta = mydata.series_of_images(file_name,
+                                                       projections_start = projections_start,
+                                                       projections_end = projections_end,
+                                                       slices_start = slices_start,
+                                                       slices_end = slices_end,
+                                                       sample_name = sample_name,
+                                                       projections_digits = 3,
+                                                       projections_zeros = True,
+                                                       log='INFO'
+                                                    )    
+    # and/or read as data exchange
+    # Read HDF5 file.
+    ##data, white, dark, theta = tomopy.xtomo_reader('/local/dataraid/databank/dataExchange/microCT/CHESS_01.h5',
+    ##                                               slices_start=100,
+    ##                                               slices_end=101)
+
+    # Xtomo object creation and pipeline of methods.  
+    d = tomopy.xtomo_dataset(log='debug')
+    d.dataset(data, white, dark, theta)
+    d.normalize()
+    d.correct_drift()
+    d.optimize_center()
+    #d.phase_retrieval()
+    #d.correct_drift()
+    #d.center=99.5
+    d.gridrec()
+
+
+    # Write to stack of TIFFs.
+    tomopy.xtomo_writer(d.data_recon, 'tmp/CHESS_scan1_', axis=0)
+
+if __name__ == "__main__":
+    main()
 
