bf8c3f5764509d66f31bd80769327b276226eb96
Francesco De Carlo <decarlof@gmail.com>
2015-04-16 00:05:28 -0500

    cleaned up

--- demo/convert_Xradia_DTU.py
+++ demo/convert_Xradia_DTU.py
@@ -20,9 +20,6 @@ Example on how to use the `xtomo_raw`_ module to read xradia raw tomographic dat
 # Data Exchange: https://github.com/data-exchange/data-exchange
 import dataexchange
 
-#import dataexchange.xtomo.formats.xradia_xrm as xradia
-#import dataexchange.xtomo.formats.data_struct as dstruct
-
 def main():
 
     file_name = '/media/DISK_02/databank/templates/xradia_dtu/sample_name.txrm'
@@ -34,25 +31,12 @@ def main():
     experimenter_email="mskan@dtu.dk"
     instrument_comment="Xradia Versa micro CT scanner"  
 
-    # Read data from file.
-#    reader = xradia.xrm()
-#    array = dstruct
-#    reader.read_txrm(file_name, array)
-
-#    print "axes", array.exchange.data_axes
-#    print "energy:", array.exchange.energy[0]
-#    print "energy units:",  array.exchange.energy_units
-#    #print "angles:", array.exchange.angles
-#    print "exposure time:", array.spectromicroscopy.data_dwell[0]
-
-
     # Read raw data
     read = dataexchange.Import()
     data, white, dark, theta = read.xtomo_raw(file_name,
-                                                       #white_file_name = white_file_name,                                                       
+                                                       #white_file_name = white_file_name,
                                                        data_type='xradia',
                                                        log='INFO'
-# NOT FINISHED YET
                                                     )    
     # Save data as dataExchange
     write = dataexchange.Export()
@@ -66,12 +50,6 @@ def main():
                           experimenter_email=experimenter_email, 
                           instrument_comment=instrument_comment,  
                           sample_name = sample_name,
-                          #sample_position_x = array.exchange.sample_position_x,
-                          #sample_position_y = array.exchange.sample_position_y,
-                          #sample_position_z = array.exchange.sample_position_z,
-                          #sample_image_shift_x = array.exchange.sample_image_shift_x,
-                          #sample_image_shift_y = array.exchange.sample_image_shift_y,
-                          #actual_pixel_size = array.exchange.actual_pixel_size,
                           data_exchange_type = 'tomography_raw_projections'
                           )
 
