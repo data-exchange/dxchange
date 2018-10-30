23bf0f967cbd17928214b7c9d1c76eb7ffd9bb09
Chen <zdms1987@hotmail.com>
2018-10-30 14:02:00 -0500

    fix one typo

--- dxchange/exchange.py
+++ dxchange/exchange.py
@@ -531,8 +531,8 @@ def read_aps_1id(fname, ind_tomo=None, proj=None, sino=None, layer=0):
     try:
         _layerdf = _metadf[_metadf['layerID'] == layer]
     except:
-        print(f"Valid layers for reconstruction are: {_metadf['layerID'].unique()]}")
-        
+        print(f"Valid layers for reconstruction are: {_metadf['layerID'].unique()}")
+
     # -- queery image data meta for given layer
     # still/projection images
     prj_start = _layerdf[_layerdf['type'] == 'still', 'nSeq'].values[0]
